eb9e885d5a185380c288ebc1009fd0cbff2d0b35
Dmitriy Krayniy <dkrayniy@entinco.com>
2022-12-16 16:01:24 +0400

    Updated pipeline scripts

--- package.ps1
+++ package.ps1
@@ -3,43 +3,68 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-$stageImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$rcImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)"
+$latestImage = "$($component.registry)/$($component.name):latest"
 
 # Build docker image
-docker build -f docker/Dockerfile -t $stageImage .
-
-if ($LastExitCode -ne 0 -and $env:RETRY -eq $true) {
-    # if package failed and retries enabled run package script again
-    Write-Host "Package failed, but retries enabled, so restarting package script again..."
-    ./package.ps1
+docker build -f "$PSScriptRoot/docker/Dockerfile" -t $rcImage -t $latestImage .
+if ($LastExitCode -eq 0) {
+    Write-Host "`nBuilt run images:`n$rcImage`n$latestImage`n"
 }
 
 # Set environment variables
-$env:IMAGE = $stageImage
+$env:IMAGE = $rcImage
 
-# Set docker machine ip (on windows not localhost)
+# Set docker ip
 if ($env:DOCKER_IP -ne $null) {
     $dockerMachineIp = $env:DOCKER_IP
 } else {
     $dockerMachineIp = "localhost"
 }
 
+# Set http port if default value overwritten
+if ($env:HTTP_PORT -ne $null) {
+    $httpPort = $env:HTTP_PORT
+} else {
+    $httpPort = "8080"
+}
+
+# Set http route to test container
+if ($env:HTTP_ROUTE -ne $null) {
+    $httpRoute = $env:HTTP_ROUTE
+} else {
+    $httpRoute = "/heartbeat"
+}
+
 try {
     # Workaround to remove dangling images
-    docker-compose -f docker/docker-compose.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" down
 
-    docker-compose -f docker/docker-compose.yml up -d
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" up -d
 
     # Test using curl
     Start-Sleep -Seconds 10
-    Invoke-WebRequest -Uri "http://$($dockerMachineIp):8080/heartbeat"
-    #$postParams = @{ org_id="123" }
-    #Invoke-WebRequest -Uri "http://$($dockerMachineIp):8080/v1/org_roles/get_organization_users" -Method POST -Body $postParams
+    Invoke-WebRequest -Uri "http://$dockerMachineIp`:$httpPort$httpRoute"
 
-    Write-Host "The container was successfully tested."
-} finally {
+    if ($LastExitCode -eq 0) {
+        Write-Host "The run container was successfully built and tested."
+    }
+}   
+catch {
+    # Output container logs if web request failed
+    $containersStatuses = docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" ps
+    # Parse docker-compose list of containers
+    foreach ($containerStatus in $containersStatuses | Select -Skip 2) {
+        $containerName = $containerStatus.split(" ")[0]
+        Write-Host "`nLogs of '$containerName' container:"
+        docker logs $containerName
+    }
+    
+    Write-Error "Error on testing run container. See logs above for more information"
+}
+finally {
     # Workaround to remove dangling images
-    docker-compose -f docker/docker-compose.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.yml" down
 }
